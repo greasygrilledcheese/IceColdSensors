ea002dcd1a6c10889c335200174a4306412e5e90
Some Guy <140758907+morroware@users.noreply.github.com>
2023-08-29 18:03:42 -0400

    Update SingleSensor.py

--- SingleSensor.py
+++ SingleSensor.py
@@ -1,6 +1,5 @@
-# Import necessary libraries
 import time
-import smbus2F
+import smbus2
 import bme280
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
@@ -11,14 +10,15 @@ from Adafruit_IO import Client
 LOG_FILE = "sensor_readings.log"
 ERROR_LOG_FILE = "error_log.log"
 
-# Function to read settings from the configuration file
 def read_settings_from_conf(conf_file):
     config = configparser.ConfigParser()
     config.read(conf_file)
     settings = {}
-    for key in ['SENSOR_LOCATION_NAME', 'MINUTES_BETWEEN_READS', 'SLACK_API_TOKEN',
-                'SLACK_CHANNEL', 'SLACK_USERS_TO_TAG', 'SENSOR_THRESHOLD_TEMP',
-                'THRESHOLD_COUNT', 'ADAFRUIT_IO_USERNAME', 'ADAFRUIT_IO_KEY']:
+    keys = ['SENSOR_LOCATION_NAME', 'MINUTES_BETWEEN_READS', 'SLACK_API_TOKEN',
+            'SLACK_CHANNEL', 'SLACK_USERS_TO_TAG', 'SENSOR_THRESHOLD_TEMP',
+            'THRESHOLD_COUNT', 'ADAFRUIT_IO_USERNAME', 'ADAFRUIT_IO_KEY',
+            'ADAFRUIT_IO_GROUP_NAME', 'ADAFRUIT_IO_TEMP_FEED', 'ADAFRUIT_IO_HUMIDITY_FEED']
+    for key in keys:
         try:
             if key in ['SENSOR_THRESHOLD_TEMP']:
                 settings[key] = config.getfloat('General', key)
@@ -33,13 +33,11 @@ def read_settings_from_conf(conf_file):
             raise
     return settings
 
-# Function to log errors
 def log_error(message):
     with open(ERROR_LOG_FILE, 'a') as file:
         timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
         file.write(f"{timestamp} - ERROR: {message}\n")
 
-# Function to log sensor readings to a file
 def log_to_file(sensor_name, temperature, humidity):
     with open(LOG_FILE, 'a') as file:
         timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
@@ -48,54 +46,38 @@ def log_to_file(sensor_name, temperature, humidity):
 # Read settings from the configuration file
 settings = read_settings_from_conf('SingleSensorSettings.conf')
 
-# Extract sensor and configuration data
-SENSOR_LOCATION_NAME = settings['SENSOR_LOCATION_NAME']
-MINUTES_BETWEEN_READS = settings['MINUTES_BETWEEN_READS']
-SLACK_API_TOKEN = settings['SLACK_API_TOKEN']
-SLACK_CHANNEL = settings['SLACK_CHANNEL']
-SLACK_USERS_TO_TAG = settings['SLACK_USERS_TO_TAG']
-SENSOR_THRESHOLD_TEMP = settings['SENSOR_THRESHOLD_TEMP']
-THRESHOLD_COUNT = settings['THRESHOLD_COUNT']
-ADAFRUIT_IO_USERNAME = settings['ADAFRUIT_IO_USERNAME']
-ADAFRUIT_IO_KEY = settings['ADAFRUIT_IO_KEY']
+# Extract settings
+for key, value in settings.items():
+    globals()[key] = value
 
-# Initialize threshold counters and alert flags
 SENSOR_ABOVE_THRESHOLD_COUNT = 0
 SENSOR_ALERT_SENT = False
 
-# Initialize BME280 sensor
 port = 1
 address_1 = 0x77
 bus_1 = smbus2.SMBus(port)
 calibration_params_1 = bme280.load_calibration_params(bus_1, address_1)
 
-# Initialize Slack client
 slack_client = WebClient(token=SLACK_API_TOKEN)
-
-# Initialize Adafruit IO client
 adafruit_io_client = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
-# Initialize sensor alert message
+
 sensor_alert_message = ""
-# Main loop for reading data, logging, and sending alerts
+
 while True:
     try:
-        # Read data from the BME280 sensor
         bme280data_1 = bme280.sample(bus_1, address_1, calibration_params_1)
         humidity_1 = format(bme280data_1.humidity, ".1f")
         temp_c_1 = bme280data_1.temperature
         temp_f_1 = (temp_c_1 * 9 / 5) + 32
-
-        # Log to file
         log_to_file(SENSOR_LOCATION_NAME, temp_f_1, humidity_1)
 
-        # Send temperature and humidity to Adafruit IO
-        adafruit_io_client.send('temperature', temp_f_1)
-        adafruit_io_client.send('humidity', humidity_1)
+        # Modified lines to use settings from config file
+        adafruit_io_client.send(f"{ADAFRUIT_IO_GROUP_NAME}.{ADAFRUIT_IO_TEMP_FEED}", temp_f_1)
+        adafruit_io_client.send(f"{ADAFRUIT_IO_GROUP_NAME}.{ADAFRUIT_IO_HUMIDITY_FEED}", humidity_1)
 
     except Exception as e:
         log_error(f"Error reading or logging data: {e}")
 
-    # Check thresholds and send alerts for the sensor
     if temp_f_1 > SENSOR_THRESHOLD_TEMP:
         SENSOR_ABOVE_THRESHOLD_COUNT += 1
         if SENSOR_ABOVE_THRESHOLD_COUNT >= THRESHOLD_COUNT and not SENSOR_ALERT_SENT:
@@ -112,7 +94,6 @@ while True:
         SENSOR_ALERT_SENT = False
         SENSOR_ABOVE_THRESHOLD_COUNT = 0
 
-    # Send alert messages to Slack
     if sensor_alert_message:
         try:
             tagged_users = " ".join(SLACK_USERS_TO_TAG)
